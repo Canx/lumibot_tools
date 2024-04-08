42a75b23a6b63b7c1a7817203baff1937c9d5b3a
Rubén Cancho <304053+Canx@users.noreply.github.com>
2024-04-08 19:11:01 +0200

    Added __init__.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="lumibot_tools",
-    version="0.1.2",
+    version="0.1.3",
     packages=find_packages(),
     install_requires=[
         'lumibot',  # Añade lumibot como una dependencia
