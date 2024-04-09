af3190fd3dfbd5c6d8a9c930ec8f4682113e5e74
Rubén Cancho <304053+Canx@users.noreply.github.com>
2024-04-09 18:25:57 +0200

    Bump version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="lumibot_tools",
-    version="0.1.4",
+    version="0.1.5",
     packages=find_packages(),
     install_requires=[
         'lumibot',  # Añade lumibot como una dependencia
