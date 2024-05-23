40db42d64db03ce91ce72ca05f76506c085d0c8f
Ruben Cancho <304053+Canx@users.noreply.github.com>
2024-05-23 18:00:08 +0200

    Added new position trend buy sizing

--- lumibot_tools/position/sizing.py
+++ lumibot_tools/position/sizing.py
@@ -2,19 +2,23 @@ class Sizing:
 
     MAX_CRYPTO_DECIMALS = 8
 
-    def __init__(self, strategy, min_cash, assets=None):
+    def __init__(self, strategy, min_cash, trend_up=None, trend_down=None, assets=None):
         self.cash = strategy.cash
         self.assets = assets
         self.min_cash = min_cash
         self.max_crypto_decimals = Sizing.MAX_CRYPTO_DECIMALS
         self.get_historical_prices = strategy.get_historical_prices
         self.log_message = strategy.log_message
+        self.trend_up = trend_up
+        self.trend_down = trend_down
 
-    def shares_to_buy(self, symbol, price_per_share, method='basic', **kwargs):
+    def shares_to_buy(self, asset, price_per_share, method='basic', **kwargs):
         if method == 'basic':
-            return self.shares_to_buy_basic(symbol, price_per_share, self.cash, **kwargs)
+            return self.shares_to_buy_basic(asset, price_per_share, self.cash, **kwargs)
         elif method == 'var':
-            return self.shares_to_buy_var(symbol, price_per_share, **kwargs)
+            return self.shares_to_buy_var(asset, price_per_share, **kwargs)
+        elif method == 'trend':
+            return self.shares_to_buy_trend(asset, price_per_share, **kwargs)
         else:
             raise ValueError("Unsupported method type")
     
@@ -50,20 +54,20 @@ class Sizing:
 
             return shares_to_buy
     
-    def shares_to_buy_trend(self, asset, price_per_share, trend_strength_function, max_percentage_per_trade=1.0, ):
+    def shares_to_buy_trend(self, asset, price_per_share, trend_up=None, max_percentage_per_trade=1.0):
         if price_per_share <= 0:
             return 0.0
         
         if self.cash < self.min_cash:
             return 0.0
         
-        available_cash = self.cash * max_percentage_per_trade * trend_strength_function(asset)
+        available_cash = self.cash * max_percentage_per_trade * trend_up(asset)
         
-        if self.assets_type == "crypto":
+        if asset.asset_type == "crypto":
             # Calcula y redondea la cantidad de criptomoneda a comprar según el máximo de decimales permitido
             shares_to_buy = available_cash / price_per_share
             shares_to_buy = round(shares_to_buy, self.max_crypto_decimals)
-        elif self.assets_type == "stock":
+        elif asset.asset_type == "stock":
             # Para acciones, redondea al número entero más cercano
             shares_to_buy = int(available_cash / price_per_share)
         else:
